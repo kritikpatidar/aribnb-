dfb328e3e749ab9203f75e00973fa1e90413dad9
kritik patidar <kritikpatidar100@gmail.com>
2024-09-25 11:18:19 +0530

    final commit2

--- router/listing.js
+++ router/listing.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Listing = require("../models/listing.js");
+// const Listing = require("../models/listing.js");
 
 const wrapAsync = require("../utility/wrapasync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
